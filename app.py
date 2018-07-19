7036eb9ad0f0843038cd93e4f90e05755acfac96
iamtol <peacekung@gmail.com>
2018-07-19 23:27:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import requests
 import re
 import random
 import configparser
+import csv
+import urllib2
 from bs4 import BeautifulSoup
 from flask import Flask, request, abort
 from imgurpython import ImgurClient
@@ -69,6 +71,12 @@ def technews():
         content += '{}\n{}\n\n'.format(title, link)
     return content
 
+def checkstatus():
+    data=urllib2.urlopen("https://s3-ap-southeast-1.amazonaws.com/mdstatus/md_status/example_j.json")
+    for content in data:
+        print (content)
+     return content
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print("event.reply_token:", event.reply_token)
@@ -80,9 +88,16 @@ def handle_message(event):
             TextSendMessage(text=content))
         return 0
     
+     if event.message.text == "server":
+        content = checkstatus()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    
     if event.message.text == "menu":
         buttons_template = TemplateSendMessage(
-            alt_text='เริ่มเล่น template',
+            alt_text='menu template',
             template=ButtonsTemplate(
                 title='เลือกบริการ',
                 text='โปรดเลือก',
@@ -94,65 +109,21 @@ def handle_message(event):
                     ),
                     MessageTemplateAction(
                         label='หนัง',
-                        text='หนัง'
+                        text='movies'
                     ),
                     MessageTemplateAction(
-                        label='อื่นๆ',
-                        text='อื่นๆ'
+                        label='สถานะ server',
+                        text='server'
                     ),
                     MessageTemplateAction(
                         label='ข่าวดัง',
-                        text='ข่าวดัง'
+                        text='news'
                     )
                 ]
             )
         )
         line_bot_api.reply_message(event.reply_token, buttons_template)
         return 0
-    if event.message.text == "ข่าว":
-        buttons_template = TemplateSendMessage(
-            alt_text='ข่าว template',
-            template=ButtonsTemplate(
-                title='ประเภทข่าว',
-                text='โปรดเลือก',
-                thumbnail_image_url='https://i.imgur.com/vkqbLnz.png',
-                actions=[
-                    MessageTemplateAction(
-                        label='ข่าวด่วนของ Apple',
-                        text='ข่าวด่วนของ Apple'
-                    ),
-                    MessageTemplateAction(
-                        label='เทค',
-                        text='เทค'
-                    ),
-                    MessageTemplateAction(
-                        label='PanX Pan เทคโนโลยี',
-                        text='PanX Pan เทคโนโลยี'
-                    )
-                ]
-            )
-        )
-        line_bot_api.reply_message(event.reply_token, buttons_template)
-        return 0
-    
-@handler.add(MessageEvent, message=StickerMessage)
-def handle_sticker_message(event):
-    print("package_id:", event.message.package_id)
-    print("sticker_id:", event.message.sticker_id)
-    # ref. https://developers.line.me/media/messaging-api/sticker_list.pdf
-    sticker_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 21, 100, 101, 102, 103, 104, 105, 106,
-                   107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125,
-                   126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 401, 402]
-    index_id = random.randint(0, len(sticker_ids) - 1)
-    sticker_id = str(sticker_ids[index_id])
-    print(index_id)
-    sticker_message = StickerSendMessage(
-        package_id='1',
-        sticker_id=sticker_id
-    )
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
     
 if __name__ == '__main__':
     app.run()
