ffaab15e3f5f8e2e70ee78fbcc2533f6b5b09e62
iamtol <peacekung@gmail.com>
2018-07-20 10:57:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def handle_message(event):
         content = status[0]
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=content)
+            TextSendMessage(text=content))
         return 0
     
     if event.message.text == "เช็คสถานะเซิฟเวอร์":
